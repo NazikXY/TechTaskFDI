26defcddff2ffd7287de187f7e168a728323463d
N_XY <n.kubliy@gmail.com>
2022-06-09 12:07:05 +0300

    a little refactoring

--- src/api/currencies.py
+++ src/api/currencies.py
@@ -8,9 +8,15 @@ from .. import models
 router = APIRouter(prefix='/currencies', tags=['currencies'], dependencies=[Depends(get_current_user)])
 
 
-@router.get('/get_currencies', response_model=models.CurrencySuccessPair)
-async def endpoint(amount: float = Query(default=1, gt=0, ), currency_service: CurrenciesService = Depends()):
-
+@router.get(
+    '/get_currencies',
+    response_model=models.CurrencySuccessPair,
+    description="Returns exchange rate of UAH to EUR and USD"
+)
+async def endpoint(
+        amount: float = Query(default=1, gt=0, ),
+        currency_service: CurrenciesService = Depends()
+):
     usd_result = await currency_service.get_rate('USD', amount)
     eur_result = await currency_service.get_rate('EUR', amount)
 
